ffa96397518657584e41e908ba376343c8aaaed9
clubpenguingit <clubpenguingit@gmail.com>
2019-03-12 13:24:11 +0100

    TrackCalculator og tilhørende tests

--- AirTrafficManager/TrackCalculator.cs
+++ AirTrafficManager/TrackCalculator.cs
@@ -6,28 +6,30 @@ using System.Threading.Tasks;
 
 namespace AirTrafficManager
 {
-    class TrackCalculator
+    public class TrackCalculator
     {
         public virtual double CalculateVelocity(int x1, int x2, int y1, int y2, int z1, int z2, int time1, int time2)
         {
-            var value = (((x2 - x1) ^ 2) + ((y2 - y1) ^ 2) + ((z2 - z1) ^ 2));
+            var value = ((Math.Pow((x2 - x1),2)) + (Math.Pow((y2 - y1), 2)) + (Math.Pow((z2 - z1), 2)));
+            
             var deltaDistance = Math.Sqrt(value);
 
             var deltaTime = time2 - time1;
 
-            return (deltaDistance / deltaTime);
+            var velocity = deltaDistance / deltaTime;
+
+            return (velocity);
         }
 
         public virtual double CalculateCourse(int x1, int x2, int y1, int y2)
         {
-            //https://www.igismap.com/formula-to-find-bearing-or-heading-angle-between-two-points-latitude-longitude/
-            var x = Math.Cos(x2) * Math.Sin(y2 - y1);
+            var m = (y2 - y1) / (x2 - x1);
 
-            var y = (Math.Cos(x1) * Math.Sin(x2)) - (Math.Sin(x1) * Math.Cos(x2) * Math.Cos(y2 - y1));
+            var courseRadians = Math.Atan(m);
 
-            var course = Math.Atan2(x, y);
+            var courseDegrees = courseRadians * (180 / Math.PI);
 
-            return course;
+            return courseDegrees;
         }
     }
 }
