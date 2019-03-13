2d8a9ba32b0d53678e6ff1d8e4d43d9093e311dd
tobiassandoelund <36983846+tobiassandoelund@users.noreply.github.com>
2019-03-13 16:46:57 +0100

    Lille test til mærkelig jenkinsfej

--- ATMSUnitTests/TransponderReceiverClientUnitTests.cs
+++ ATMSUnitTests/TransponderReceiverClientUnitTests.cs
@@ -9,7 +9,7 @@ using TransponderReceiver;
 
 namespace ATMSUnitTests
 {
-    [TestFixture()]
+    [TestFixture]
     public class TransponderReceiverClientUnitTests
     {
         private TransponderReceiverClient uut;
@@ -28,32 +28,26 @@ namespace ATMSUnitTests
             uut = new TransponderReceiverClient(fakeReceiver, fakeCalculator);
         }
 
-        [TearDown]
-        public void TearDown()
+
+        [Test]
+        public void TransponderReceiverClient_fakeReceiverRaisesEvent_uutReceivesEvent()
         {
-            uut = null; 
+            // Create testdata
+            List<string> testData = new List<string>();
+            testData.Add("ATR423;39045;12932;14000;20151006213456789");
+            testData.Add("BCD123;10005;85890;12000;20151006213456789");
+            testData.Add("XYZ987;25059;75654;4000;20151006213456789");
+
+            int numberOfEventsReceived = 0;
+            uut.DataReceivedEvent += (object s, DataEventArgs e) =>
+            {
+                numberOfEventsReceived++;
+            };
+            // Act
+            fakeReceiver.TransponderDataReady += Raise.EventWith(this, new RawTransponderDataEventArgs(testData));
+            // Assert
+            Assert.That(numberOfEventsReceived, Is.EqualTo(1));
         }
-
-
-        //[Test]
-        //public void TransponderReceiverClient_fakeReceiverRaisesEvent_uutReceivesEvent()
-        //{
-        //    // Create testdata
-        //    List<string> testData = new List<string>();
-        //    testData.Add("ATR423;39045;12932;14000;20151006213456789");
-        //    testData.Add("BCD123;10005;85890;12000;20151006213456789");
-        //    testData.Add("XYZ987;25059;75654;4000;20151006213456789");
-
-        //    int numberOfEventsReceived = 0;
-        //    uut.DataReceivedEvent += (object s, DataEventArgs e) =>
-        //    {
-        //        numberOfEventsReceived++;
-        //    };
-        //    // Act
-        //    fakeReceiver.TransponderDataReady += Raise.EventWith(this, new RawTransponderDataEventArgs(testData));
-        //    // Assert
-        //    Assert.That(numberOfEventsReceived, Is.EqualTo(1));
-        //}
 
         [Test]
         public void TransponderReceiverClient_fakeReceiverRaisesSpecificData_uutCreatesTrack()
@@ -63,12 +57,8 @@ namespace ATMSUnitTests
             testData.Add("ATR423;39045;12932;14000;20151006213456789");
 
             Track resultTrack = null;
-            Track correctTrack = new Track("ATR423", 
-                                            39045, 
-                                            12932, 
-                                            14000, 
-                                            new DateTime(2015,10,06,
-                                                21,34,
+            Track correctTrack = new Track("ATR423", 39045,12932, 14000, 
+                                            new DateTime(2015,10,06, 21,34,
                                                 56,789),0, 0 );
             uut.DataReceivedEvent += (object s, DataEventArgs e) =>
             {
