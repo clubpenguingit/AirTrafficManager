2ca479b984b20129cada9b44191901a342c167fa
tobiassandoelund <36983846+tobiassandoelund@users.noreply.github.com>
2019-03-17 13:17:49 +0100

    AirTrafficManagementSystem event oprettet, opdaterer tracks (tid, hastighed, koordinater) og sender dem videre.
    Mulig bug i at finde allerede eksisterende tracks.

--- AirTrafficManager/AirTrafficManagerSystem/AirTrafficManagementSystem.cs
+++ AirTrafficManager/AirTrafficManagerSystem/AirTrafficManagementSystem.cs
@@ -17,30 +17,73 @@ namespace AirTrafficManager
 
         private MonitoredAirspace _monitoredAirspace;
         private ITransponderReceiverClient _transPondRecClient;
+        private List<Track> _airCraftsInAirspaceList;
+        private TrackCalculator _trackCalculator;
 
         public event EventHandler<ATMSEventArgs> DataReady;
 
-        public AirTrafficManagementSystem(MonitoredAirspace monair,  ITransponderReceiverClient transclient)
+        public AirTrafficManagementSystem(MonitoredAirspace monair,  ITransponderReceiverClient transclient,
+                                            TrackCalculator calc)
         {
             this._monitoredAirspace = monair;
             this._transPondRecClient = transclient;
+            this._trackCalculator = calc;
             _transPondRecClient.DataReceivedEvent += OnReceiverClientEvent;
+            _airCraftsInAirspaceList = new List<Track>();
         }
         
 
         //When ReceiverClient events
         private void OnReceiverClientEvent(object sender, DataEventArgs e)
         {
-            // TODO: do
-            var airCraftsInAirspaceList = new List<Track>();
 
             foreach (var track in e.Tracks)
             {
-                if (_monitoredAirspace.ValidateAirspace(track))
-                {
+                // Look for track in list of tracks in airspace
+                var foundTrack = _airCraftsInAirspaceList.Find(ByTag(track.Tag));
 
+                // If already in list - update speed and coordinates.
+                if (foundTrack != null)
+                {
+                    var velocity = _trackCalculator.CalculateVelocity(track.XCoordinate, foundTrack.XCoordinate,
+                        track.YCoordinate, foundTrack.YCoordinate,
+                        track.Altitude, foundTrack.Altitude,
+                        track.Time.Second, foundTrack.Time.Second); // Second might have to change to ToADate e.g.
+
+                    var compassCourse = _trackCalculator.CalculateCourse(track.XCoordinate, foundTrack.YCoordinate,
+                        track.YCoordinate, foundTrack.YCoordinate);
+
+                    var newTrack = new Track(foundTrack.Tag, foundTrack.XCoordinate, foundTrack.YCoordinate,
+                        foundTrack.Altitude, track.Time, velocity, compassCourse);
+                }
+                // If not in list - add it    This should be tested - intellisense says expression is alwais true
+                else if (foundTrack == null)
+                {
+                    _airCraftsInAirspaceList.Add(track);
+                }
+                // If outside airspace, remove it. 
+                bool inAirSpace = _monitoredAirspace.ValidateAirspace(track);
+                if (!inAirSpace)
+                {
+                    _airCraftsInAirspaceList.Remove(track);
                 }
             }
+
+            // Pass on Tracks in monitored airspace. 
+            var args = new ATMSEventArgs();
+            args.Tracks = _airCraftsInAirspaceList;
+
+            // Raise event if somebody has "subscribed" to it
+            DataReady?.Invoke(this, args);
+        }
+
+        // List.Find helper
+        Predicate<Track> ByTag(string tag)
+        {
+            return delegate(Track track)
+            { 
+                return track.Tag == tag;
+            };
         }
     }
 }
